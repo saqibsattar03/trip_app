eec919ff819643ab10c974217dd59ed14e2fd937
saqibsattar03 <ranasaqib734@gmail.com>
2021-12-04 13:43:59 +0500

    'demo_page_added'

---
+++ lib/ui/pages/demo_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoPage extends StatelessWidget {
+  const DemoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("demo"),
+        backgroundColor: Colors.indigo,
+      ),
+    );
+  }
+}
