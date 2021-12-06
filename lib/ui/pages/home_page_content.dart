dfbe0307eccb190c7c5684a612b818aa67c6f838
saqibsattar03 <ranasaqib734@gmail.com>
2021-12-06 14:17:42 +0500

    'tab_bar_added'

--- lib/ui/pages/home_page_content.dart
+++ lib/ui/pages/home_page_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trip_app/ui/widgets/app_title_text.dart';
 
 class HomePageContent extends StatefulWidget {
   const HomePageContent({Key? key}) : super(key: key);
@@ -7,12 +8,98 @@ class HomePageContent extends StatefulWidget {
   _HomePageContentState createState() => _HomePageContentState();
 }
 
-class _HomePageContentState extends State<HomePageContent> {
+class _HomePageContentState extends State<HomePageContent>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('home page content'),
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 70.0,
+                left: 20.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    size: 30.0,
+                  ),
+                  Container(
+                    width: 60.0,
+                    height: 60.0,
+                    margin: const EdgeInsets.only(right: 20.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      // color: Colors.grey.withOpacity(0.5),
+                    ),
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20.0),
+              child: AppTitleText(
+                text: 'Discover',
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  labelColor: Colors.black,
+                  labelPadding: const EdgeInsets.only(
+                    right: 20.0,
+                    left: 20.0,
+                  ),
+                  unselectedLabelColor: Colors.grey,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  isScrollable: true,
+                  controller: _tabController,
+                  tabs: [
+                    Tab(
+                      text: "Places",
+                    ),
+                    Tab(
+                      text: "Inspirations",
+                    ),
+                    Tab(
+                      text: "Emotions",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 300.0,
+              width: double.maxFinite,
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Text('hi'),
+                  Text("there"),
+                  Text('bye'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
